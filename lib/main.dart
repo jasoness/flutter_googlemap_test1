979103c86a9f05b345391eb14b1134d620239335
jasoness <danyoh9844@gmail.com>
2021-09-01 17:59:48 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('GoogleMap'),
+      ),
+      body: MapSample(),
+    ),
+  ),
+  );
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.56421135, 127.0016985),
+    zoom: 10,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+
+      target: LatLng(37.56421135, 127.0016985),
+      zoom: 10);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('현위치'),
+        icon: Icon(Icons.person),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
+
